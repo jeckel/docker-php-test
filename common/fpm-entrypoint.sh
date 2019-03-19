f8a4e0406198f23214c38d594acac195d523cfac
Julien Mercier-Rojas <devci@j3ck3l.me>
2019-03-19 14:39:53 +0100

    :truck: Split entrypoint cli/fpm

---
+++ common/fpm-entrypoint.sh
@@ -0,0 +1,12 @@
+#!/usr/bin/env sh
+
+# Store command line args
+CMD_ARGS=$@
+set -e
+
+# Fix www-data user to have same UID than the mounted folder
+set -- `ls -nd .` && LOCAL_UID=$3 && LOCAL_GID=$4
+usermod -u ${LOCAL_UID} www-data
+groupmod -g ${LOCAL_GID} www-data
+
+exec ${CMD_ARGS}
