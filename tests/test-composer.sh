5e9337856c80b7ae73cffd52e689a35b6b891218
Julien Mercier-Rojas <devci@j3ck3l.me>
2019-03-20 10:17:28 +0100

    :bug: Replace bash par sh

--- tests/test-composer.sh
+++ tests/test-composer.sh
@@ -1,14 +1,14 @@
-#!/usr/bin/env bash
+#!/usr/bin/env sh
 
-if [[ ! -z $1 ]]; then
+if [ ! -z $1 ]; then
     IMAGE_TAG=$1
 fi
-if [[ -z ${IMAGE_TAG} ]]; then
+if [ -z ${IMAGE_TAG} ]; then
     echo "Missing env var IMAGE_TAG"
     exit 1
 fi
 
-if [[ -d vendor ]]; then
+if [ -d vendor ]; then
     rm -Rf vendor
 fi
 docker run --rm -it -v `pwd`:/project -w /project php-test:${IMAGE_TAG} composer install
@@ -21,11 +21,11 @@ VENDOR_UID=$(stat -c %u vendor)
 VENDOR_GID=$(stat -c %g vendor)
 
 # Check that generated files have the same owner
-if [[ ${VENDOR_UID} != ${LOCAL_UID} ]]; then
+if [ ${VENDOR_UID} != ${LOCAL_UID} ]; then
     echo "Error, invalid folder owner, expected ${LOCAL_UID}, got ${VENDOR_UID}"
     exit 1
 fi
-if [[ ${VENDOR_GID} != ${LOCAL_GID} ]]; then
+if [ ${VENDOR_GID} != ${LOCAL_GID} ]; then
     echo "Error, invalid folder group owner, expected ${LOCAL_GID}, got ${VENDOR_GID}"
     exit 1
 fi
