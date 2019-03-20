eaa29d33bc02dfe605f4bbad28eb0454c3e74052
Julien Mercier-Rojas <devci@j3ck3l.me>
2019-03-20 11:16:57 +0100

    :bug: Fix root user

--- tests/test-composer.sh
+++ tests/test-composer.sh
@@ -21,6 +21,7 @@ if [ `whoami` = root ]; then
         mkdir -p /home/bob && \
         chown -R bob:bob .
     fi
+    chmod +x test-composer.sh
     sudo -u 1000:1000 -E sh "./test-composer.sh ${IMAGE_TAG}"
     exit $?
 fi
