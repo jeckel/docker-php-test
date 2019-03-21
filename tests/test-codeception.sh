38e24a1179e9bbec56c0c5905ff3f90537ade93d
Julien Mercier-Rojas <devci@j3ck3l.me>
2019-03-21 09:51:11 +0100

    :white_check_mark: Add test script for codeception images

--- tests/test-codeception.sh
+++ tests/test-codeception.sh
@@ -1,1 +1,18 @@
 #!/usr/bin/env bash
+
+set -e
+
+TAG=$1
+if [ -z ${TAG} ]; then
+    echo "Missing tag name"
+    exit 1
+fi
+
+if [ -d codecept ]; then
+    rm -Rf codecept
+fi
+
+mkdir -p codecept
+cd codecept
+docker run --rm -it -v `pwd`:/app -w /app php-test:${TAG} codecept bootstrap
+cd ..
