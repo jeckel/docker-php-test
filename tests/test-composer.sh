d8e4bbe2f36dfd4a2cda5f5f90a4ae2b5dab022f
Julien Mercier-Rojas <devci@j3ck3l.me>
2019-03-22 09:26:31 +0100

    :white_check_mark: Add composer test script

---
+++ tests/test-composer.sh
@@ -0,0 +1,20 @@
+#!/usr/bin/env bash
+
+#!/usr/bin/env bash
+
+set -e
+
+TAG=$1
+if [ -z ${TAG} ]; then
+    echo "Missing tag name"
+    exit 1
+fi
+
+if [ -d somposer ]; then
+    rm -Rf composer
+fi
+
+mkdir -p composer
+cd composer
+docker run --rm -it -v `pwd`:/app -w /app php-test:${TAG} composer init
+cd ..
