ec91730a0eb5d437911684d6b9350dc3d7190f5e
Julien Mercier-Rojas <devci@j3ck3l.me>
2019-04-08 13:38:52 +0200

    :bug: Fix cli entrypoint when running as root

--- common/cli-entrypoint.sh
+++ common/cli-entrypoint.sh
@@ -4,13 +4,30 @@
 CMD_ARGS=$@
 set -e
 
-# Fix www-data user to have same UID than the mounted folder
+# Get running user
+CURRENT_UID=$(id -u)
+CURRENT_GID=$(id -g)
+
+# Get working directory owner
 set -- `ls -nd .` && LOCAL_UID=$3 && LOCAL_GID=$4
-usermod -u ${LOCAL_UID} www-data
-groupmod -g ${LOCAL_GID} www-data
 
-mkdir -p ${COMPOSER_HOME} && chown -R www-data:www-data ${COMPOSER_HOME}
 
-# Run command with the correct rights
-sudo -g \#${LOCAL_GID} -u \#${LOCAL_UID} -E sh -c "${CMD_ARGS}"
+# Current user UID same as mounted folder owner
+if [[ ${LOCAL_UID} = ${CURRENT_UID} ]]; then
+    if [[ ! -z ${COMPOSER_HOME} ]]; then
+        mkdir -p ${COMPOSER_HOME}
+    fi
 
+    exec ${CMD_ARGS}
+else
+    # Fix www-data user to have same UID than the mounted folder
+    usermod -u ${LOCAL_UID} www-data
+    groupmod -g ${LOCAL_GID} www-data
+
+    if [[ ! -z ${COMPOSER_HOME} ]]; then
+        mkdir -p ${COMPOSER_HOME} && chown -R www-data:www-data ${COMPOSER_HOME}
+    fi
+
+    # Run command with the correct rights
+    sudo -g \#${LOCAL_GID} -u \#${LOCAL_UID} -E sh -c "${CMD_ARGS}"
+fi
