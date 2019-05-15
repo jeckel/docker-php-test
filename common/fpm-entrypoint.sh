cfcefe46896bd16849dbce629f7fbb2c5481976b
Julien Mercier-Rojas <devci@j3ck3l.me>
2019-05-15 09:38:21 +0200

    :sparkles: Upgrade forceing user in fpm

--- common/fpm-entrypoint.sh
+++ common/fpm-entrypoint.sh
@@ -5,6 +5,10 @@ CMD_ARGS=$@
 CMD=$1
 set -e
 
+# Get running user
+CURRENT_UID=$(id -u)
+CURRENT_GID=$(id -g)
+
 # Get working directory owner
 set -- `ls -nd .` && WORKDIR_UID=$3 && WORKDIR_GID=$4
 
@@ -12,23 +16,53 @@ set -- `ls -nd .` && WORKDIR_UID=$3 && WORKDIR_GID=$4
 LOCAL_UID=${LOCAL_UID:-${WORKDIR_UID}}
 LOCAL_GID=${LOCAL_GID:-${WORKDIR_GID}}
 
-printf "==> \033[0;33mLOCAL_UID: \033[0;32m\033[1m${LOCAL_UID}\033[0m | \033[0;33mLOCAL_GID: \033[0;32m\033[1m${LOCAL_GID}\033[0m\n"
 
-# Fix www-data user to have same UID than the mounted folder
-if ! $(getent passwd ${LOCAL_UID}); then
-    usermod -u ${LOCAL_UID} www-data
+if ${ENTRYPOINT_DEBUG}; then
+    printf " --> \033[0;33mCURRENT_UID: \033[0;32m\033[1m${CURRENT_UID}\033[0m | \033[0;33mCURRENT_GID: \033[0;32m\033[1m${CURRENT_GID}\033[0m\n"
+    printf " --> \033[0;33mLOCAL_UID: \033[0;32m\033[1m${LOCAL_UID}\033[0m | \033[0;33mLOCAL_GID: \033[0;32m\033[1m${LOCAL_GID}\033[0m\n"
 fi
 
-# Try to fix www-data group to have same GID then the mounted folder (if not already in use)
-if [[ $(cat /etc/group | grep ":${LOCAL_GID}:" | wc -l) = 1 ]]; then
-    LOCAL_GID=${LOCAL_UID}
-fi
-groupmod -g ${LOCAL_GID} www-data
+# Current user UID same as mounted folder owner
+if [ "${LOCAL_UID}" = "${CURRENT_UID}" ]; then
+    if [ ! -z ${COMPOSER_HOME} ]; then
+        mkdir -p ${COMPOSER_HOME}
+    fi
 
-mkdir -p ${COMPOSER_HOME} && chown -R www-data:www-data ${COMPOSER_HOME}
-
-if [ "${CMD}" == "php-fpm" ]; then
-    exec ${CMD_ARGS}
+    if [ "${CMD}" == "php-fpm" ]; then
+        if ${ENTRYPOINT_DEBUG}; then
+            printf " --> \033[0;33mexec \033[0;32m\033[1m${CMD_ARGS}\033[0;33m\033[0m\n"
+        fi
+        exec ${CMD_ARGS}
+    else
+        if ${ENTRYPOINT_DEBUG}; then
+            printf " --> \033[0;33mexec sh -c \"\033[0;32m\033[1m${CMD_ARGS}\033[0;33m\"\033[0m\n"
+        fi
+        exec sh -c "${CMD_ARGS}"
+    fi
 else
-    sudo -g \#${LOCAL_GID} -u \#${LOCAL_UID} -E sh -c "${CMD_ARGS}"
+
+    # Fix www-data user to have same UID than the mounted folder
+    if ! $(getent passwd ${LOCAL_UID}); then
+        usermod -u ${LOCAL_UID} www-data
+    fi
+
+    # Try to fix www-data group to have same GID then the mounted folder (if not already in use)
+    if [[ $(cat /etc/group | grep ":${LOCAL_GID}:" | wc -l) = 1 ]]; then
+        LOCAL_GID=${LOCAL_UID}
+    fi
+    groupmod -g ${LOCAL_GID} www-data
+
+    mkdir -p ${COMPOSER_HOME} && chown -R www-data:www-data ${COMPOSER_HOME}
+
+    if [ "${CMD}" == "php-fpm" ]; then
+        if ${ENTRYPOINT_DEBUG}; then
+            printf " --> \033[0;33mexec \033[0;32m\033[1m${CMD_ARGS}\033[0;33m\033[0m\n"
+        fi
+        exec ${CMD_ARGS}
+    else
+        if ${ENTRYPOINT_DEBUG}; then
+            printf " --> \033[0;33msudo -g \#\033[0;32m\033[1m${LOCAL_GID}\033[0;33m -u \#\033[0;32m\033[1m${LOCAL_UID}\033[0;33m -E sh -c \"\033[0;32m\033[1m${CMD_ARGS}\033[0;33m\"\033[0m\n"
+        fi
+        sudo -g \#${LOCAL_GID} -u \#${LOCAL_UID} -E sh -c "${CMD_ARGS}"
+    fi
 fi
